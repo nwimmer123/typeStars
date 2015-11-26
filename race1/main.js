6e154352c53093cf5ac31f8b41063af69b41f94a
Noah Wimmer <nwimmer123@yahoo.com>
2015-11-25 22:30:52 -0800

    added win condition

--- race1/main.js
+++ race1/main.js
@@ -2,10 +2,6 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
  
-
-
-
-
 //p1 moves using keystroke l
 
 $(document).keydown(function(e) {
@@ -22,23 +18,25 @@ $(document).keydown(function(e) {
 		}
 	});
 
+
+
 //working RESET button
 	$("button").on("click", function() {
 		$(".one").css({left: 0,});
 		$(".two").css({left: 0,});
-		
-	
 		});
 
 //win condition
- /*
-
+ 
+	$(document).keydown(function(e) {
+		if ($(".one").css("left") === ("800px")) {
+			alert("Player one wins");
+		} else if ($(".two").css("left") === ("800px")) {
+			alert("Player two wins");
+		}
 	});
 
-	if ($(".one").css("left") === ("20px")) {
-		alert("winner");
-	}
-*/
-
 });
+
+
 
