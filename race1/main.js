0150c3c0a2feed74a8af89526478686c67aacae2
Noah Wimmer <nwimmer123@yahoo.com>
2015-11-26 08:47:46 -0800

    VERY simple game1 done

--- race1/main.js
+++ race1/main.js
@@ -3,22 +3,18 @@ console.log("Sanity Check: JS is working!");
 $(document).ready(function(){
  
 //p1 moves using keystroke l
-
-$(document).keydown(function(e) {
-        if(e.keyCode == 76) {
-        $(".one").css({left: "+=5px",});
-		}
-	});
+	$(document).keydown(function(e) {
+		if(e.keyCode == 76) {
+			$(".one").css({left: "+=5px",});
+			}
+		});
 
 //p2 moves using keystroke
-
-$(document).keydown(function(e) {
-        if(e.keyCode == 65) {
-        $(".two").css({left: "+=5px",});
-		}
-	});
-
-
+	$(document).keydown(function(e) {
+		if(e.keyCode == 65) {
+			$(".two").css({left: "+=5px",});
+			}
+		});
 
 //working RESET button
 	$("button").on("click", function() {
@@ -27,38 +23,21 @@ $(document).keydown(function(e) {
 		});
 
 //win condition
- 
-var winner;
-
 	$(document).keydown(function(e) {
-		if ($(".one").css("left") === ("20px")) {
-			winner = "Player 1";
+		if ($(".one").css("left") === ("800px")) {
 			alert("Player one wins");
 			winRamifications();
-		} else if ($(".two").css("left") === ("20px")) {
-			winner = "Player 2";
+		} else if ($(".two").css("left") === ("800px")) {
 			alert("Player two wins");
 			winRamifications();
 		}
 	});
 
 //autoresets board
-var playerOneWins = 0;
-var playerTwoWins = 0;
-
 	function winRamifications() {
 		$(".one").css({left: 0,});
 		$(".two").css({left: 0,});
-		if (winner === "Player 1") {
-			playerOneWins += 1;
-		} else if (winner === "Player 2") {
-			playerTwoWins += 1;
-		}
 	}
-	
-
 
 });
-
-
 
