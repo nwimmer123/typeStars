bba4dd758e8c005a943bba76c238082f71186c4e
Noah Wimmer <nwimmer123@yahoo.com>
2015-11-27 10:26:10 -0800

    added random word generator

--- race2/randomWords.js
+++ race2/randomWords.js
@@ -300,3 +300,35 @@
     "disseminule",
     "omentum",
     ];
+
+
+ //I grabbed this function of stackOverflow after researching for awhile.
+
+ function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+shuffle(words);
+var gameWords = [];
+
+for(var i = 0; i < 50; i ++) {
+    gameWords.push(words[i]);
+ }
+
+ console.log(gameWords);
+
