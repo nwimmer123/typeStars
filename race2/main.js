def72e7163f399635a3b38cd70e515232ef94dce
Noah Wimmer <nwimmer123@yahoo.com>
2015-11-28 12:02:09 -0800

    added win counter and variable wins

--- race2/main.js
+++ race2/main.js
@@ -1,17 +1,22 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
- 
-//p1 moves using keystroke l
+ 	
+ 	var playerOneName = prompt("Player one, what is your name?");
+ 	var playerTwoName = prompt("Player two, what is your name?");
+ 	$(".pOne").text(playerOneName + " the jaguar!!");
+ 	$(".pTwo").text(playerTwoName+ " the panther!!");
+
+//p1 moves using keystroke a
 	$(document).keydown(function(e) {
-		if(e.keyCode == 76) {
+		if(e.keyCode == 65) {
 			$(".one").css({left: "+=5px",});
 			}
 		});
 
-//p2 moves using keystroke
+//p2 moves using keystroke l
 	$(document).keydown(function(e) {
-		if(e.keyCode == 65) {
+		if(e.keyCode == 76) {
 			$(".two").css({left: "+=5px",});
 			}
 		});
@@ -22,14 +27,24 @@ $(document).ready(function(){
 		$(".two").css({left: 0,});
 		});
 
+	$(".winCounter").hide();
+
+	var pOneWins = 0;
+	var pTwoWins = 0;
+
+
 //win condition
 	$(document).keydown(function(e) {
-		if ($(".one").css("left") === ("700px")) {
-			alert("Player one wins");
+		if ($(".one").css("left") === ("100px")) {
 			winRamifications();
-		} else if ($(".two").css("left") === ("700px")) {
-			alert("Player two wins");
+			pOneWins += 1;
+			$(".counterOne").text("number of glorious victories: " + pOneWins);
+			$(".counterOne").show();
+		} else if ($(".two").css("left") === ("100px")) {
 			winRamifications();
+			pTwoWins +=1;
+			$(".counterTwo").text("number of glorious victories: " + pTwoWins);
+			$(".counterTwo").show();
 		}
 	});
 
