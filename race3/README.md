bf246e3d0cb30350363b0edcb001fca61f8d581c
Noah Wimmer <nwimmer123@yahoo.com>
2015-11-29 21:20:20 -0800

    race3 done

--- race3/README.md
+++ race3/README.md
@@ -1,8 +1,8 @@
 README.md
 
 Racing Game
-l moves player 2
-a moves player 1
+k,l,o,, moves player 2
+a,s,z,w moves player 1
 
 whoever crosses the board first gets a point
 First one to 3 points wins the game
@@ -15,14 +15,11 @@ jquery
 
 Existing Features
 
-ok backgrounds and layput
-win counter that is initially hidden and resets after win
-you can enter your name
+same as prior games +
+players are objects that score their name and score
+floating blackhole of death that zaps everyone back to start if you touch it
 
 Planned Features
 
-have a field where you can enter players names which will then disappear
-be able to pick an animal of your chosing from a field which also disappears
-make it so the rematch alert doesn't appear after the final win
-this game is boring!!! 
-make it so you are a bird and you can move in multiple directions, when you get with in range of a predator that suddenly appears in front of you via show function ftriggered ny proximity, you must evade it or die. 
+make it so only the player who is zapped goes back to start, but I'm to tired to make it work at this point
+add avatars that can be choosen from and store them in the player object as well
--- race3/main.js
+++ race3/main.js
@@ -2,22 +2,23 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-var playerOne = {
-	name: "player1",
-	avatar: "",
-	score: 0,
-};
+	var playerOne = {
+		name: "player1",
+		score: 0,
+	};
 
-var playerTwo = {
-	name: "player2",
-	avatar: "",
-	score: 0,
-};
+	var playerTwo = {
+		name: "player2",
+		score: 0,
+	};
  	
- 	playerOne.name = prompt("Player one, what is your name?");
- 	playerTwo.name = prompt("Player two, what is your name?");
- 	$(".pOne").text(playerOne.name + " the jaguar!!");
- 	$(".pTwo").text(playerTwo.name + " the panther!!");
+	$(document).keyup(function(e) {
+		if(e.keyCode == 13) {
+			playerOne.name = $(".oneName").val();
+			playerTwo.name = $(".twoName").val();
+			$("form").hide();
+			}
+	});
 
 //p1 moves left using keystroke a
 	$(document).keyup(function(e) {
@@ -76,21 +77,17 @@ var playerTwo = {
 
 	$(".winCounter").hide();
 
-	var pOneWins = 0;
-	var pTwoWins = 0;
-
-
 //win condition
 	$(document).keydown(function(e) {
-		if ($(".one").css("left") === ("100px")) {
-			pOneWins += 1;
+		if ($(".one").css("left") === ("700px")) {
+			playerOne.score += 1;
 			winRamifications();
-			$(".counterOne").text("number of glorious victories: " + pOneWins);
+			$(".counterOne").text(playerOne.name + "'s score:" + playerOne.score);
 			$(".counterOne").show();
-		} else if ($(".two").css("left") === ("100px")) {
-			pTwoWins +=1;
+		} else if ($(".two").css("left") === ("700px")) {
+			playerTwo.score +=1;
 			winRamifications();
-			$(".counterTwo").text("number of glorious victories: " + pTwoWins);
+			$(".counterTwo").text(playerTwo.name + "'s score:" + playerTwo.score);
 			$(".counterTwo").show();
 		}
 	});
@@ -100,26 +97,25 @@ var playerTwo = {
 		$(".one").css({left: 0,});
 		$(".two").css({left: 0,});
 		alert("Prepare for a rematch!!");
-		if (pOneWins === 3 || pTwoWins === 3){
-			if (pOneWins === 3) {
-				pOneWins = 0;
-				pTwoWins = 0;
+		if (playerOne.score === 3 || playerTwo.score === 3){
+			if (playerOne.score === 3) {
+				playerOne.score = 0;
+				playerTwo.score = 0;
 				$(".winCounter").hide();
-				alert(playerOneName + " has vanquished all!!!");	
-			} else if(pTwoWins === 3) {
-				pOneWins = 0;
-				pTwoWins = 0;
+				alert(playerOne.name + " has vanquished all!!!");	
+			} else if(playerTwo.score === 3) {
+				playerOne.score = 0;
+				playerTwo.score = 0;
 				$(".winCounter").hide();
-				alert(playerTwoName + " has vanquished all!!!");	
+				alert(playerTwo.name + " has vanquished all!!!");	
 			}
 		}
 	}
-/*
+
 // i snagged all the following functions off of Stack Overflow, but they do 
 //mostly make sense to me
     animateDiv();
-    animateDiv2();
-    
+
 
 
 function makeNewPosition(){
@@ -146,16 +142,7 @@ function animateDiv(){
     
 };
 
-function animateDiv2(){
-    var newq = makeNewPosition();
-    var oldq = $('.enemy2').offset();
-    var speed = calcSpeed([oldq.top, oldq.left], newq);
-    
-    $('.enemy2').animate({ top: newq[0], left: newq[1] }, speed, function(){
-      animateDiv();        
-    });
-    
-};
+
 function calcSpeed(prev, next) {
     
     var x = Math.abs(prev[1] - next[1]);
@@ -190,46 +177,17 @@ function calcSpeed(prev, next) {
       if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) {
           return false;
       } else {
-      console.log("collision!!")
-      $(".one").css({left: 0,});
-      return true;
-      }
-  }
-
-//detect collision for every 2 seconds
-window.setInterval(function() {
-    $('#result2').text(collision($('.one'), $('.enemy')));
-}, 50);
-
-    function collision($div1, $div2) {
-      var x1 = $div1.offset().left;
-      var y1 = $div1.offset().top;
-      var h1 = $div1.outerHeight(true);
-      var w1 = $div1.outerWidth(true);
-      var b1 = y1 + h1;
-      var r1 = x1 + w1;
-      var x2 = $div2.offset().left;
-      var y2 = $div2.offset().top;
-      var h2 = $div2.outerHeight(true);
-      var w2 = $div2.outerWidth(true);
-      var b2 = y2 + h2;
-      var r2 = x2 + w2;
-        
-      if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) {
-          return false;
-      } else {
-      console.log("collision!!")
       $(".player").css({left: 0,});
       return true;
       }
   }
 
-//detect collision for every 2 seconds
-window.setInterval(function() {
-    $('#result2ssss').text(collision($('.two'), $('.enemy2')));
-}, 50);
+  	window.setInterval(function() {
+    $('#result').text(collision($('.player'), $('.enemy')));
+	}, 50);
 
-*/
+
+
 });
 
 
