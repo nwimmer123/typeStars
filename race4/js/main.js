c2c9fca441765ba757387df62646cce46f04e43b
Noah Wimmer <nwimmer123@yahoo.com>
2016-09-13 10:29:41 -0700

    reorganized functions

--- race4/js/main.js
+++ race4/js/main.js
@@ -35,23 +35,87 @@ $(document).ready(function(){
 
 });
 
+//sets number of players for the game and error check bad input
+var numPlayers = 0;
+function playerSelector() {
+  if ($("#playerCount").val() > 4) {
+    $(lowerIntro).text("Please enter a number less then 5");
+  } else if ($("#playerCount").val() < 1) {
+    $(lowerIntro).text("Please enter a number greater then 0");
+  } else {
+    numPlayers = $("#playerCount").val();
+    $("#intro").hide();
+    $("#avatar").show();
+  }
+};
+
+// ** PLAYER CREATION FUNCTIONS ** \\
+
+var playerCounter = 1;
+var tempPlayer = "Player 1";
+function multiPlayerCreation() { 
+  if (playerCounter < numPlayers) {
+    createPlayer();
+    playerCounter += 1;
+    resetPlayerChoice();
+  } else {
+    createPlayer();
+    allCreated();
+  }
+};
+
+//sets players avatar
+var id = "spaceship1";
+function createAvatarId() {
+  $(".avatars").css('background-color', 'white');
+  id = $(this).attr("id");
+  $(this).css('background-color', 'yellow');
+};
+
+//creates individual player object
+var playerInfo = [];
+function createPlayer () {
+  playerInfo.push({
+    player_id: tempPlayer, 
+    name: $("#playerName").val(),
+    avatar: id,
+    score:[],
+  });
+};
+
+//clears player creation div
+var currentPlayer = 1;
+function resetPlayerChoice() {
+  $("#playerName").val("");
+  $(".avatars").css('background-color', 'white');
+  currentPlayer += 1;
+  tempPlayer = "Player " + currentPlayer;
+  $("#numPlayers").text(tempPlayer);
+};
+
+//hides selection div and reveals instruvtion div
+function allCreated() {
+  $("#avatar").hide();
+  $("#instructions").show();
+  $("#start").show();
+  console.log(playerInfo);
+};
+
+// ** WORD ARRAY CREATOR FUNCTIONS ** \\
+
 //randomize word array order
 function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex ;
-
   // While there remain elements to shuffle...
   while (0 !== currentIndex) {
-
     // Pick a remaining element...
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
-
     // And swap it with the current element.
     temporaryValue = array[currentIndex];
     array[currentIndex] = array[randomIndex];
     array[randomIndex] = temporaryValue;
   }
-
   return array;
 }
 
@@ -64,19 +128,63 @@ function generateWords() {
    }
 }
 
-//game timer
-var timeoutID;
-function timer() {
-  timeoutID = window.setTimeout(endRace, 45000)
+// ** RUNNING GAME FUNCTIONS ** \\
+
+//hides instructions and reveals game
+function setupRace(i) {
+  $("#word").show();
+  document.getElementById("word").focus();
+  generateWords();
+  $(".toType").text(gameWords[0]);
+  $("#instructions").hide();
+  $(".wordDisplay").show();
+  $(".player").show();
+  var playerAvatar = 'url("images/' + playerInfo[i].avatar + '.png")'
+  $(".player").css({background: playerAvatar});
 }
 
+//game timer
+var timeoutID;
+function timer(i) {
+  timeoutID = window.setTimeout(endRace, 3000)
+}
+
+//advances ship on correct typing
+function runRace() {
+  for (var i = 0; i < playerInfo.length; i++) {
+    setupRace(i);
+    timer();
+    var j = 1;
+    $(document).keyup(function(e){
+      var targetWord = $(".toType").text();
+      var typedWord = $("#word").val();
+      while (j < gameWords.length){
+        if(typedWord === targetWord){
+          $(".player").css({left: "+=15px",});
+          targetWord = $(".toType").text(gameWords[j]);
+          $("#word").val("");
+          j++;
+        }else {
+          return
+        };
+      }
+    });
+  };
+}
+
+// ** END GAME FUNCTIONS ** \\
+
 //hides race div and generates score
-function endRace() {
+function endRace(i) {
   $(".wordDisplay").hide();
   $("#word").hide();
   alert("Times Up!");
   generateScore();
   generateWinMessage();
+  if (i < playerInfo.length) {
+    $("#start").show();
+    
+  }
   winDisplay();
   $("#endGame").show();
   $("#gameReset").show();
@@ -109,96 +217,25 @@ function winDisplay() {
   $("#finalScore").text(scoreMessage);
 }
 
-var id = "spaceship1";
-function createAvatarId() {
-  $(".avatars").css('background-color', 'white');
-  id = $(this).attr("id");
-  $(this).css('background-color', 'yellow');
-};
+//BACK UP SINGLE PLAYER WORKING RUN RACE FUNCTION
+// function runRace() {
 
-var numPlayers = 0;
-function playerSelector() {
-  if ($("#playerCount").val() > 4) {
-    $(lowerIntro).text("Please enter a number less then 5");
-  } else if ($("#playerCount").val() < 1) {
-    $(lowerIntro).text("Please enter a number greater then 0");
-  } else {
-    numPlayers = $("#playerCount").val();
-    $("#intro").hide();
-    $("#avatar").show();
-  }
-};
+//   setupRace();
+//   timer();
+//   var j = 1;
+//   $(document).keyup(function(e){
+//     var targetWord = $(".toType").text();
+//     var typedWord = $("#word").val();
+//     while (j < gameWords.length){
+//       if(typedWord === targetWord){
+//         $(".player").css({left: "+=15px",});
+//         targetWord = $(".toType").text(gameWords[j]);
+//         $("#word").val("");
+//         j++;
+//       }else {
+//         return
+//       };
+//     }
+//   });
 
-var playerCounter = 1;
-var tempPlayer = "Player 1";
-function multiPlayerCreation() { 
-  if (playerCounter < numPlayers) {
-    createPlayer();
-    playerCounter += 1;
-    resetPlayerChoice();
-  } else {
-    createPlayer();
-    allCreated();
-  }
-};
-
-var currentPlayer = 1;
-function resetPlayerChoice() {
-  $("#playerName").val("");
-  $(".avatars").css('background-color', 'white');
-  currentPlayer += 1;
-  tempPlayer = "Player " + currentPlayer;
-  $("#numPlayers").text(tempPlayer);
-};
-
-//hides slection div and reveals instruvtion div
-function allCreated() {
-  $("#avatar").hide();
-  $("#instructions").show();
-  $("#start").show();
-  console.log(playerInfo);
-};
-
-var playerInfo = [];
-function createPlayer () {
-  playerInfo.push({
-    player_id: tempPlayer, 
-    name: $("#playerName").val(),
-    avatar: id,
-    score:[],
-  });
-};
-
-//hides instructions and reveals game
-function setupRace() {
-  $("#word").show();
-  document.getElementById("word").focus();
-  generateWords();
-  $(".toType").text(gameWords[0]);
-  $("#instructions").hide();
-  $(".wordDisplay").show();
-  $(".player").show();
-  var playerAvatar = 'url("images/' + playerInfo[0].avatar + '.png")'
-  $(".player").css({background: playerAvatar});
-}
-
-//advances ship on correct typing
-function runRace() {
-  setupRace();
-  timer();
-  var j = 1;
-  $(document).keyup(function(e){
-    var targetWord = $(".toType").text();
-    var typedWord = $("#word").val();
-    while (j < gameWords.length){
-      if(typedWord === targetWord){
-        $(".player").css({left: "+=15px",});
-        targetWord = $(".toType").text(gameWords[j]);
-        $("#word").val("");
-        j++;
-      }else {
-        return
-      };
-    }
-  });
-}
+// }
